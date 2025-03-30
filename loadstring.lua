845b63548d3a3d119a2758d8e7f93e319a9c6df0
Bigcoderais <153544801+Amhim123hd@users.noreply.github.com>
2025-03-29 20:21:17 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,7 @@
 -- Loadstring for Combined Aimbot Script
 -- To use this, replace YOUR_REPOSITORY with your repository name
 
-local scriptUrl = "https://raw.githubusercontent.com/Amhim123hd/YOUR_REPOSITORY/main/combined_aimbot.lua"
+local scriptUrl = "https://raw.githubusercontent.com/Amhim123hd/-LAWSUIT-UNIVERSAL/main/combined_aimbot.lua"
 
 -- Load and execute the script from GitHub
 loadstring(game:HttpGet(scriptUrl))()
